b593c53c36766e839f656b3a94cd5bc56e85db7c
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-04-28 06:44:09 +0700

    Add parameter and default value for adding delay

--- app/src/main/java/CommonUtils.kt
+++ app/src/main/java/CommonUtils.kt
@@ -4,14 +4,13 @@ import android.os.HandlerThread
 object CommonUtils {
     lateinit var handler: Handler
 
-    fun runOnDefaultThread(runnable: () -> Unit) {
+    fun runOnDefaultThread(runnable: () -> Unit, delayMillis: Long = 0) {
         if (!::handler.isInitialized) {
             val handlerThread = HandlerThread("DefaultNormalThread", Thread.NORM_PRIORITY)
             handlerThread.start()
 
             handler = Handler(handlerThread.looper)
         }
-
-        handler.post(runnable)
+        handler.postDelayed(runnable, delayMillis)
     }
 }
