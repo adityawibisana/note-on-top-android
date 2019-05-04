3c65fb202782bbf746ece610476b59e5f26cbd8c
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-05-05 02:20:31 +0700

    Revert "Change text directly from the note"
    This reverts commit d21d00fc0a12d0239e0a5f7302aa994c57ed7101.

--- app/src/main/java/CommonUtils.kt
+++ app/src/main/java/CommonUtils.kt
@@ -1,15 +1,6 @@
-import android.app.Activity
-import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
-import android.os.Looper
-import android.os.SystemClock
 import android.util.Log
-import android.view.MotionEvent
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
-import kotlinx.android.synthetic.main.view_main_editable.view.*
 
 object CommonUtils {
     private const val TAG = "CommonUtils"
@@ -38,24 +29,6 @@ object CommonUtils {
     fun runOnDefaultThread(runnable: () -> Unit, delayMillis: Long = 0) {
         handler.postDelayed(runnable, delayMillis)
     }
-
-    fun runOnUIThread(runnable: () -> Unit, delayMillis: Long = 0) {
-        Handler(Looper.getMainLooper()).postDelayed(runnable, delayMillis)
-    }
-
-
-    fun forceShowKeyboard(context: Context, view: View) {
-        CommonUtils.runOnUIThread({
-            view.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN , 0.0f, 0.0f, 0))
-            view.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP , 0.0f, 0.0f, 0))
-        }, 70)
-    }
-
-    fun hideKeyboard(context: Context, view: View) {
-        val inputMethodManager = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
-        inputMethodManager!!.hideSoftInputFromWindow(view.windowToken, 0)
-    }
-
 
     fun runOnUnusedThread (runnable: () -> Unit) {
         handlers[12].post {
--- app/src/main/java/com/aw/ontopnote/model/event/SwitchEditModeEvent.kt
+++
@@ -1,3 +0,0 @@
-package com.aw.ontopnote.model.event
-
-data class SwitchEditModeEvent (val isEditable: Boolean)
