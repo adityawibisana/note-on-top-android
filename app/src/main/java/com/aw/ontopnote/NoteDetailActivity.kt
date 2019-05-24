4771bc41c21e0fcfbdbc67e59590b182ac3f79f8
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-05-24 15:54:39 +0700

    Decorate textView

--- app/src/main/java/com/aw/ontopnote/NoteDetailActivity.kt
+++ app/src/main/java/com/aw/ontopnote/NoteDetailActivity.kt
@@ -2,15 +2,21 @@ package com.aw.ontopnote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.widget.TextView
 import com.aw.ontopnote.model.Note
 import com.aw.ontopnote.model.NoteRepository
+import com.aw.ontopnote.view.DefaultTextView
 import kotlinx.android.synthetic.main.activity_note_detail.*
 
 class NoteDetailActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_NOTE_ID = "extraNoteId"
+    }
+
+    private val defaultTextView: DefaultTextView by lazy {
+        DefaultTextView.getInstance(this)
     }
 
     private lateinit var note: Note
@@ -22,7 +28,7 @@ class NoteDetailActivity : AppCompatActivity() {
         CommonUtils.runOnDefaultThread({
             note = NoteRepository.getNoteById(MainApp.applicationContext(), intent.getStringExtra(EXTRA_NOTE_ID))
             val noteTextView = tv_note as TextView
-            noteTextView.text = note.content
+            defaultTextView.decorateTextView(noteTextView, note)
         })
     }
 }
