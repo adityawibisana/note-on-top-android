4a4818a94f4cbbbf253c7e459285d93b1b57542c
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-06-02 15:34:24 +0700

    Fix drawoverlay using workaround

--- app/src/main/java/com/aw/ontopnote/MainActivity.kt
+++ app/src/main/java/com/aw/ontopnote/MainActivity.kt
@@ -2,6 +2,7 @@ package com.aw.ontopnote
 
 import CommonUtils
 import CommonUtils.runOnDefaultThread
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -16,6 +17,13 @@ import androidx.lifecycle.Observer
 import com.aw.ontopnote.model.Note
 import com.aw.ontopnote.model.NoteRepository
 import kotlinx.android.synthetic.main.default_action_bar.*
+import android.graphics.PixelFormat
+import android.view.WindowManager
+import android.content.Context.WINDOW_SERVICE
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkDrawOverlayPermission()
+        showPermissionOrProceedToApp()
 
         runOnDefaultThread({
             firstNote = NoteRepository.getOrCreateFirstNote(applicationContext)
@@ -54,21 +62,56 @@ class MainActivity : AppCompatActivity() {
         **/
     }
 
-    private fun checkDrawOverlayPermission() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            startService(Intent(applicationContext, MainService::class.java))
-        } else if (Settings.canDrawOverlays(this)) {
-            onActivityResult(REQUEST_CODE, -1, null)
+    /**
+     * Workaround for Android O that always return true when checking via Settings.canDrawOverlays
+     */
+    private fun canDrawOverlays(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true
+        else if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            return Settings.canDrawOverlays(this)
         } else {
-            val uri = Uri.parse("package:$packageName")
-            startActivityForResult(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri), REQUEST_CODE)
+            if (Settings.canDrawOverlays(this)) return true
+            try {
+                val mgr = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                val viewToAdd = View(this)
+                val params = WindowManager.LayoutParams(
+                    0,
+                    0,
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                    else
+                        WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSPARENT
+                )
+                viewToAdd.layoutParams = params
+                mgr.addView(viewToAdd, params)
+                mgr.removeView(viewToAdd)
+                return true
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return false
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+        if (requestCode == REQUEST_CODE) {
+            showPermissionOrProceedToApp()
+        }
+    }
+
+    private fun showPermissionOrProceedToApp() {
+        if (canDrawOverlays()) {
             startService(Intent(applicationContext, MainService::class.java))
+        } else {
+            val uri = Uri.parse("package:$packageName")
+            startActivityForResult(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri), REQUEST_CODE)
+            Toast.makeText(this, R.string.allow_draw_over_other_app_permission, Toast.LENGTH_LONG).show()
         }
     }
 
