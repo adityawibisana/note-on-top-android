5ce735fc4fc7bcc095442b454a0500cb9b317e95
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-07-15 09:46:33 +0700

    Base activity will implement kotlin coroutines lib

--- app/src/main/java/com/aw/ontopnote/BaseActivity.kt
+++ app/src/main/java/com/aw/ontopnote/BaseActivity.kt
@@ -7,12 +7,23 @@ import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aw.ontopnote.helper.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 @SuppressLint("Registered")
-open class BaseActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity(), CoroutineScope {
     companion object {
         const val REQUEST_CODE = 1
     }
+
+    private val job: Job by lazy { Job() }
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     override fun onResume() {
         super.onResume()
@@ -26,15 +37,20 @@ open class BaseActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
     private fun showPermissionOrProceedToApp() {
-        CommonUtils.runOnDefaultThread({
-            if (Utils.canDrawOverlays(this)) {
+        launch (Default) {
+            if (Utils.canDrawOverlays(this@BaseActivity)) {
                 startService(Intent(applicationContext, MainService::class.java))
             } else {
                 val uri = Uri.parse("package:$packageName")
                 startActivityForResult(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri), REQUEST_CODE)
-                Toast.makeText(this, R.string.allow_draw_over_other_app_permission, Toast.LENGTH_LONG).show()
+                Toast.makeText(this@BaseActivity, R.string.allow_draw_over_other_app_permission, Toast.LENGTH_LONG).show()
             }
-        })
+        }
     }
 }
