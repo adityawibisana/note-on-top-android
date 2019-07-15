c1bf7c5286e49c7c14de190b7b225c3d36fac54f
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-07-15 09:53:39 +0700

    Replace NoteRepository db runOnDefaultThread with launch

--- app/src/main/java/com/aw/ontopnote/model/NoteRepository.kt
+++ app/src/main/java/com/aw/ontopnote/model/NoteRepository.kt
@@ -6,15 +6,26 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import com.aw.ontopnote.R
 import com.aw.ontopnote.model.event.UpdateNoteEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Job
 import org.greenrobot.eventbus.EventBus
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-object NoteRepository {
+object NoteRepository: CoroutineScope {
     private const val TAG = "NoteRepository"
 
+    private val job: Job by lazy { Job() }
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
     fun insertNote(context: Context, note: Note) {
-        CommonUtils.runOnDefaultThread({
+        launch (Default) {
             NotesDatabase.getInstance(context).noteDao().insertNote(note)
-        })
+        }
     }
 
     fun getOrCreateFirstNote(context: Context) : Note {
@@ -37,13 +48,13 @@ object NoteRepository {
     }
 
     fun updateNote(context: Context, note: Note) {
-        CommonUtils.runOnDefaultThread({
+        launch (Default) {
             val noteDao = NotesDatabase.getInstance(context).noteDao()
             noteDao.update(note)
 
             Log.v(TAG, "updated note with id:" + note.id)
             EventBus.getDefault().post(UpdateNoteEvent(note))
-        })
+        }
     }
 
     fun getAllNotes(context: Context): List<Note>  {
