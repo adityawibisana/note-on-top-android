ff83a4dc3e97970f4728ee10e0aa1c11174c0278
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-10-29 19:40:32 +0700

    Add sample code to connect to our local websocket

--- app/src/main/java/com/aw/ontopnote/network/SocketManager.kt
+++ app/src/main/java/com/aw/ontopnote/network/SocketManager.kt
@@ -7,25 +7,28 @@ import org.json.JSONObject
 import android.util.Log
 
 const val TAG = "SocketManager"
-const val socketURL = "http://192.168.1.101:1337?__sails_io_sdk_version=1.2.1"
+const val socketURL = "http://192.168.1.101:1337"
+const val version_query = "__sails_io_sdk_version=1.2.1"
 
 object SocketManager  {
     private val socket: Socket by lazy {
         val opts = IO.Options()
         opts.transports = arrayOf(WebSocket.NAME)
 
-        IO.socket(socketURL, opts)
+        IO.socket("$socketURL?$version_query", opts)
     }
 
     fun connect() {
         socket.on(Socket.EVENT_CONNECT) {
             JSONObject().also {
-                it.put("url", "http://192.168.1.101:1337/ping?__sails_io_sdk_version=1.2.1&data1=yoloooooooo")
+                it.put("url", "$socketURL/ping?$version_query&data1=yoloooooooo")
                 socket.emit("get", it)
             }
 
         }.on(Socket.EVENT_ERROR) {
             Log.v(TAG, "Socket connect error:")
+        }.on(Socket.EVENT_DISCONNECT) {
+            Log.v(TAG, "Socket disconnect")
         }
 
         socket.connect()
