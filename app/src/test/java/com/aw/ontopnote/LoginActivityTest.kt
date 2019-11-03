8547bb9f47359cd331ae6c71ffa63b02baa4d37c
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-11-03 11:03:26 +0700

    Add test to ensure no crash upon wrong enter username or password

--- app/src/test/java/com/aw/ontopnote/LoginActivityTest.kt
+++ app/src/test/java/com/aw/ontopnote/LoginActivityTest.kt
@@ -7,8 +7,7 @@ import org.junit.Test
 import org.robolectric.Robolectric
 import org.junit.runner.RunWith
 import com.aw.ontopnote.util.SharedPref
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.*
 import org.junit.Before
 
 
@@ -37,4 +36,37 @@ class LoginTest {
         assertEquals(emailEditText.text.toString(), SharedPref.email)
         assertNotNull(SharedPref.token)
     }
+
+    @Test
+    fun login_with_wrong_username_should_not_crash() {
+        val activity = Robolectric.setupActivity(LoginActivity::class.java)
+
+        val loginButton = activity.findViewById<Button>(R.id.bt_login)
+        val emailEditText = activity.findViewById<EditText>(R.id.et_email)
+        val passwordEditText = activity.findViewById<EditText>(R.id.et_password)
+
+        emailEditText.setText("not an email username")
+        passwordEditText.setText("1234")
+        loginButton.performClick()
+
+        Thread.sleep(1000)
+        assertNull(SharedPref.token)
+    }
+
+    @Test
+    fun login_with_no_password_should_not_crash() {
+        val activity = Robolectric.setupActivity(LoginActivity::class.java)
+
+        val loginButton = activity.findViewById<Button>(R.id.bt_login)
+        val emailEditText = activity.findViewById<EditText>(R.id.et_email)
+
+        emailEditText.setText("a@bc.com")
+        loginButton.performClick()
+
+        Thread.sleep(1000)
+        assertNull(SharedPref.token)
+    }
+
+    @Test
+    fun should
 }
