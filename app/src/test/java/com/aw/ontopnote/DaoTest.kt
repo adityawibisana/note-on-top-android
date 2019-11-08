8841907ecd410f190ac05e3aadefeccd8e7f8de5
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-11-09 04:13:43 +0700

    Adding dao test for insert

---
+++ app/src/test/java/com/aw/ontopnote/DaoTest.kt
@@ -0,0 +1,35 @@
+package com.aw.ontopnote
+
+import androidx.test.platform.app.InstrumentationRegistry
+import com.aw.ontopnote.model.NoteRepository
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class DaoTest {
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Test
+    fun insert() {
+        runBlocking {
+            val firstNote = NoteRepository.getOrCreateFirstNote(context)
+            assertNotNull(firstNote)
+
+            val firstNoteDB = NoteRepository.getNoteById(context, firstNote.id)
+
+            assertEquals(firstNote.id, firstNoteDB.id)
+            assertEquals(firstNote.text, firstNoteDB.text)
+            assertEquals(firstNote.color, firstNoteDB.color)
+            assertEquals(firstNote.viewType, firstNoteDB.viewType)
+            assertEquals(firstNote.fontSize, firstNoteDB.fontSize)
+            assertEquals(firstNote.textColor, firstNoteDB.textColor)
+
+            println("Executed through here")
+        }
+
+    }
+}
