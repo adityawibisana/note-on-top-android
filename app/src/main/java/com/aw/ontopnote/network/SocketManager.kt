8306276afe3935d06ea670c98beea77c26d68fcf
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-11-27 13:18:29 +0700

    Add timeout and suspend for connect

--- app/src/main/java/com/aw/ontopnote/network/SocketManager.kt
+++ app/src/main/java/com/aw/ontopnote/network/SocketManager.kt
@@ -6,18 +6,23 @@ import io.socket.engineio.client.transports.WebSocket
 import org.json.JSONObject
 import android.util.Log
 import com.aw.ontopnote.util.SharedPref
+import kotlinx.coroutines.suspendCancellableCoroutine
 import java.lang.Exception
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 const val TAG = "SocketManager"
 const val socketURL = "http://192.168.1.101:1337"
 const val query_version = "__sails_io_sdk_version=1.2.1"
 
 object SocketManager  {
+    const val TIMEOUT = 7000L
     var isConnected = false
 
     val socket: Socket by lazy {
         val opts = IO.Options()
         opts.transports = arrayOf(WebSocket.NAME)
+        opts.timeout = TIMEOUT
 
         IO.socket("$socketURL?$query_version&token=${SharedPref.token}", opts).apply {
             this.on(Socket.EVENT_CONNECT) {
@@ -28,6 +33,7 @@ object SocketManager  {
                 }
             }.on(Socket.EVENT_ERROR) {
                 Log.v(TAG, "Socket connect error:")
+                handleDisconnectEvent()
             }.on(Socket.EVENT_DISCONNECT) {
                 Log.v(TAG, "Socket disconnect")
                 handleDisconnectEvent()
@@ -50,8 +56,18 @@ object SocketManager  {
         "token=" + SharedPref.token
     }
 
-    fun connect() {
-        socket.connect()
+    suspend fun connect() : Boolean = suspendCancellableCoroutine { continuation ->
+        if (SharedPref.token != null && !isConnected) {
+            socket.once(Socket.EVENT_CONNECT) {
+                continuation.resume(true)
+            }
+            socket.connect()
+            CommonUtils.runOnDefaultThread({
+                if (continuation.isActive) continuation.resume(isConnected)
+            }, TIMEOUT)
+        } else {
+            continuation.resume(false)
+        }
     }
 
     fun handleDisconnectEvent() {
