f48b84834c3027b97130473d0f218f5faca0ba83
Aditya Wibisana <aditya.wibisana@gmail.com>
2019-11-30 15:54:19 +0700

    Test the viewmodel

--- app/src/test/java/com/aw/ontopnote/LoginActivityTest.kt
+++ app/src/test/java/com/aw/ontopnote/LoginActivityTest.kt
@@ -1,13 +1,14 @@
 package com.aw.ontopnote
 
-import android.widget.Button
 import android.widget.EditText
 import androidx.test.runner.AndroidJUnit4
 import org.junit.Test
 import org.robolectric.Robolectric
 import org.junit.runner.RunWith
 import com.aw.ontopnote.util.SharedPref
+import com.aw.ontopnote.viewmodel.LoginViewModel
 import junit.framework.Assert.*
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 
 
@@ -19,51 +20,47 @@ class LoginTest {
     }
 
     @Test
-    @Throws(Exception::class)
-    fun login() {
-        val activity = Robolectric.setupActivity(LoginActivity::class.java)
+    fun login() = runBlocking {
+        val vm = LoginViewModel()
 
-        val loginButton = activity.findViewById<Button>(R.id.bt_login)
-        val emailEditText = activity.findViewById<EditText>(R.id.et_email)
-        val passwordEditText = activity.findViewById<EditText>(R.id.et_password)
+        val email = "a@bc.com"
+        val pass = "1234"
+        vm.login(email, pass)
 
-        emailEditText.setText("a@bc.com")
-        passwordEditText.setText("1234")
-        loginButton.performClick()
-
-        Thread.sleep(1000)
-
-        assertEquals(emailEditText.text.toString(), SharedPref.email)
+        assertEquals(email, SharedPref.email)
         assertNotNull(SharedPref.token)
+
+        println("login() process done")
     }
 
     @Test
-    fun login_with_wrong_username_should_not_crash() {
+    fun login_with_wrong_username_should_not_crash() = runBlocking{
         val activity = Robolectric.setupActivity(LoginActivity::class.java)
 
-        val loginButton = activity.findViewById<Button>(R.id.bt_login)
         val emailEditText = activity.findViewById<EditText>(R.id.et_email)
         val passwordEditText = activity.findViewById<EditText>(R.id.et_password)
 
         emailEditText.setText("not an email username")
         passwordEditText.setText("1234")
-        loginButton.performClick()
 
-        Thread.sleep(1000)
+        val vm = LoginViewModel()
+        vm.login(emailEditText.text.toString(), passwordEditText.text.toString())
+
         assertNull(SharedPref.token)
+        println("login_with_wrong_username_should_not_crash() process done")
     }
 
     @Test
-    fun login_with_no_password_should_not_crash() {
+    fun login_with_no_password_should_not_crash() = runBlocking {
         val activity = Robolectric.setupActivity(LoginActivity::class.java)
 
-        val loginButton = activity.findViewById<Button>(R.id.bt_login)
         val emailEditText = activity.findViewById<EditText>(R.id.et_email)
-
         emailEditText.setText("a@bc.com")
-        loginButton.performClick()
 
-        Thread.sleep(1000)
+        val vm = LoginViewModel()
+        vm.login(emailEditText.text.toString(), "")
+
         assertNull(SharedPref.token)
+        println("login_with_no_password_should_not_crash() process done")
     }
 }
