dd8be3d9663489fa852f349aca9fd075047d06b1
Aditya Wibisana <aditya.wibisana@gmail.com>
2021-03-11 19:28:06 +0700

    Update scope util: set to coroutinescope

--- app/src/main/java/com/aw/commons/ScopeUtil.kt
+++ app/src/main/java/com/aw/commons/ScopeUtil.kt
@@ -1,12 +1,32 @@
 package com.aw.commons
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
 object ScopeUtil {
-    val ui = Job() + Dispatchers.Main
-    val disk = Job() + Dispatchers.IO
-    val network = Job() + Dispatchers.IO
-    val cpu = Job() + Dispatchers.Default
+    val ui = object: CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = Job() + Dispatchers.Main
+    }
 
+    val disk = object: CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = Job() + Dispatchers.IO
+    }
+
+    val db = object: CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = Job() + Dispatchers.IO
+    }
+    val network = object: CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = Job() + Dispatchers.IO
+    }
+
+    val cpu = object: CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = Job() + Dispatchers.Default
+    }
 }
