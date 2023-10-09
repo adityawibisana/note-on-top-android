5be6ebd32bfa382bebde5cb37692ed875a4fd811
Aditya Wibisana <aditya.wibisana@gmail.com>
2023-10-09 17:35:57 +0700

    Remove unused code

--- app/src/main/java/com/aw/ontopnote/MainService.kt
+++ app/src/main/java/com/aw/ontopnote/MainService.kt
@@ -1,34 +1,20 @@
 package com.aw.ontopnote
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.IBinder
 import android.view.WindowManager
-import androidx.core.app.NotificationCompat
 import com.aw.commons.ScopeUtils
 import com.aw.ontopnote.model.NoteRepository
-import com.aw.commons.AndroidUIHelper
 import com.aw.ontopnote.view.ViewManager
 import kotlinx.coroutines.launch
 
-const val ID = 1
-const val channelId = "OnTopNoteChannelId"
-const val channelName = "OnTopNoteChannelName"
 class MainService : Service() {
     lateinit var viewManager: ViewManager
 
-
     override fun onCreate() {
         super.onCreate()
-        startForeground(ID, getNotification())
-
-        // initialize uihelper
-        AndroidUIHelper.getInstance(this)
 
         ScopeUtils.db.launch {
             val lastEditedNote = NoteRepository.getLastEditedNoteLive(MainApp.applicationContext())
@@ -45,28 +31,5 @@ class MainService : Service() {
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
-    }
-
-    private fun getNotification(): Notification {
-        val channelId = createNotificationChannel(channelId, channelName)
-
-        return NotificationCompat.Builder(this, channelId)
-            .setOngoing(true)
-            .setSmallIcon(R.drawable.ic_launcher)
-            .setContentTitle(getString(R.string.app_name))
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setCategory(Notification.CATEGORY_SERVICE)
-            .build()
-    }
-
-    private fun createNotificationChannel(channelId: String, channelName: String): String{
-        val chan = NotificationChannel(channelId,
-            channelName, NotificationManager.IMPORTANCE_DEFAULT)
-        chan.lightColor = Color.BLUE
-        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
-        chan.setShowBadge(false)
-        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        service.createNotificationChannel(chan)
-        return channelId
     }
 }
